90df0b08a90fba13a85538e93fb4ea6e7d4b3eba
Ritesh Maurya <riteshkumar411552@gmail.com>
2023-09-07 12:16:00 +0530

    storing blogs in an array

--- src/component/Blog.js
+++ src/component/Blog.js
@@ -1,26 +1,32 @@
-import { useState, useEffect } from "react";
+import { useState} from "react";
 
 
 export default function Blog() {
   
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
+    // const [title, setTitle] = useState('');
+    // const [content, setContent] = useState('');
+    const [formData, setFormData] = useState({title:'', content:''});
+    const [blogs, setBlogs] = useState([]);
 
-    function handleSubmit  (e)  {
-       e.target.preventDefault();
+    function handleSubmit (e)  {
+       e.preventDefault()
+       setBlogs([{title:formData.title, content:formData.content}, ...blogs]);
+       console.log(blogs);
+       setFormData({title:"", content:""});
+
     }
 
     return (
         <>
         <div style={{backgroundColor:'orange', width:'50%', height:'300px', padding:'10px', margin:'auto', borderRadius:'5px'}}>
            <form onSubmit={handleSubmit}>
-            <label for='title'>Title<br/>
-                <input id='title' type='text' placeholder="Enter the title here" value={title} onChange={(e) => setTitle(e.target.value)}/><br/>
+            <label>Title<br/>
+                <input id='title' type='text' placeholder="Enter the title here" value={formData.title} onChange={(e) => setFormData({title:e.target.value, content:formData.content})}/><br/>
             </label>
             <hr/>
 
-            <label for='content'>Content<br/>
-                <textarea id='content' placeholder="content goes here.." rows='6' cols='70' value={content} onChange={(e) => setContent(e.target.value)}></textarea>
+            <label>Content<br/>
+                <textarea id='content' placeholder="content goes here.." rows='6' cols='70' value={formData.content} onChange={(e) => setFormData({title:formData.title, content:e.target.value})}></textarea>
             </label><br/>
             <hr/>
 
@@ -30,8 +36,15 @@ export default function Blog() {
            
         </div>
         <h2>Blog:</h2>
-        <h3>{title}</h3>
-        <h3>{content}</h3>
+        {blogs.map((blog) => {
+            return (
+                <div className="blog">
+                <h3>{blog.title}</h3>
+                <p>{blog.content}</p>
+                </div>
+            )
+        })}
+        
         
         </>
     )
