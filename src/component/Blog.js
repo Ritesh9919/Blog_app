b9684688b9927db25ba7354ad9b19d49734422bf
Ritesh Maurya <riteshkumar411552@gmail.com>
2023-09-07 20:55:24 +0530

    focus on the input field and setting this document title

--- src/component/Blog.js
+++ src/component/Blog.js
@@ -1,56 +1,72 @@
-import { useState} from "react";
+import { useState, useRef, useEffect } from "react";
 
 
 export default function Blog() {
-  
+
     // const [title, setTitle] = useState('');
     // const [content, setContent] = useState('');
-    const [formData, setFormData] = useState({title:'', content:''});
+    const [formData, setFormData] = useState({ title: '', content: '' });
     const [blogs, setBlogs] = useState([]);
+    const titleRef = useRef(null);
 
-    function handleSubmit (e)  {
-       e.preventDefault()
-       setBlogs([{title:formData.title, content:formData.content}, ...blogs]);
-       console.log(blogs);
-       setFormData({title:"", content:""});
+
+    useEffect(() => {
+        titleRef.current.focus();
+    }, []);
+
+    useEffect(() => {
+        if (blogs.length && blogs[0].title) {
+            document.title = blogs[0].title;
+        } else {
+            document.title = 'No blogs';
+        }
+    }, [blogs])
+
+
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        setBlogs([{ title: formData.title, content: formData.content }, ...blogs]);
+        titleRef.current.focus();
+        setFormData({ title: "", content: "" });
 
     }
 
     function handleDelete(i) {
-       setBlogs(blogs.filter((blog, index) => index !== i));
+        setBlogs(blogs.filter((blog, index) => index !== i));
     }
 
     return (
         <>
-        <div style={{backgroundColor:'orange', width:'50%', height:'300px', padding:'10px', margin:'auto', borderRadius:'5px'}}>
-           <form onSubmit={handleSubmit}>
-            <label>Title<br/>
-                <input id='title' type='text' placeholder="Enter the title here" value={formData.title} onChange={(e) => setFormData({title:e.target.value, content:formData.content})}/><br/>
-            </label>
-            <hr/>
+            <div className="form-container">
+                <form onSubmit={handleSubmit}>
+                    <label>Title<br />
+                        <input id='title' type='text' placeholder="Enter the title here" value={formData.title} ref={titleRef} onChange={(e) => setFormData({ title: e.target.value, content: formData.content })} /><br />
+                    </label>
+                    <hr />
 
-            <label>Content<br/>
-                <textarea id='content' placeholder="content goes here.." rows='6' cols='70' value={formData.content} onChange={(e) => setFormData({title:formData.title, content:e.target.value})}></textarea>
-            </label><br/>
-            <hr/>
+                    <label>Content<br />
+                        <textarea id='content' placeholder="content goes here.." rows='6' cols='70' value={formData.content} required onChange={(e) => setFormData({ title: formData.title, content: e.target.value })}></textarea>
+                    </label><br />
+                    <hr />
 
-            <button>Add</button>
-           </form>
+                    <button>Add</button>
+                </form>
 
-           
-        </div>
-        <h2>Blog:</h2>
-        {blogs.map((blog, i) => {
-            return (
-                <div className="blog" key={i}>
-                <h3>{blog.title}</h3>
-                <p>{blog.content}</p>
-                <button onClick={() => handleDelete(i)}>Delete</button>
-                </div>
-            )
-        })}
-        
-        
+
+            </div>
+            <h2>Blog:</h2>
+            {blogs.map((blog, i) => {
+                return (
+                    <div className="blog" key={i}>
+                        <h3>{blog.title}</h3>
+                        <p>{blog.content}</p>
+                        <button onClick={() => handleDelete(i)}>Delete</button>
+                    </div>
+                )
+            })}
+
+
         </>
     )
 }
